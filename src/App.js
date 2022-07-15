ee80309a206810383d7aa2243ef74aeffde07a07
Sestaks Antons <antonsh@hotmail.com>
2022-07-15 13:38:52 +0100

    Fixed browser tab name and icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { useEffect } from 'react';
 import { Navbar } from './components';
 import { Diploma , Examples, Weapons, Shopping } from './containers';
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Ant's game portfolio"
+  },[]) ;
+
   return (
     <div className="App">
       <div className='gradient__bg'>
